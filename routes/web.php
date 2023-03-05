90fe8e17f9fd8fa3374d60c6a97033da2639707a
Ryan Hoang <ryanhoang38@gmail.com>
2023-03-05 11:33:12 -0600

    create realtor index page

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\UserAccountController;
-use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +29,10 @@ Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
 
 Route::resource('user-account', UserAccountController::class)
     ->only(['create', 'store']);
+
+Route::prefix('realtor')
+    ->name('realtor.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::resource('listing', RealtorListingController::class);
+    });
